852514c58bc2f49793241898985168f484afe0e2
Olga Krasnova <krasolga2018@gmail.com>
2020-12-08 20:13:58 +0400

    Лабораторная работа по веб-сервисам

---
+++ server/db.config.js
@@ -0,0 +1,8 @@
+// Основная информация о базе данных, к которой подключается веб-приложение
+module.exports = {
+  HOST: "std-mysql.ist.mospolytech.ru",
+  USER: "std_709",
+  PASSWORD: "krasnova",
+  PORT: 3306,
+  DB: "std_709",
+};
